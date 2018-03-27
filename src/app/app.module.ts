1fa672194d262ef1d0192f2195c80a7f0cc8c685
DSallee <dsallee.dev@gmail.com>
2018-03-27 22:04:58 +0200

    servers component modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    ServerComponent
   ],
   imports: [
     BrowserModule,
